c9fcabfde826ac0753ca04e071d68b9fcb04fb86
Douglas Lowder <douglowder@mac.com>
2022-09-02 17:57:35 -0700

    EAS changes

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 
 import openSansTtf from './assets/OpenSans-VariableFont.ttf';
@@ -12,6 +12,8 @@ export default function App() {
   const [fontsLoaded, fontError] = useFonts(fonts);
 
   return (
-    <Text>Loaded: {fontsLoaded ? 'true' : 'false'}.  Error: {fontError ? String(fontError) : 'N/A'}</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Loaded: {fontsLoaded ? 'true' : 'false'}.  Error: {fontError ? String(fontError) : 'N/A'}</Text>
+    </View>
   );
 }
